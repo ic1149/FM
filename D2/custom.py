3d87c82c380fee1cf1655fc9fe73661aea193cf7
ic <chowisaac99@gmail.com>
2026-03-26 22:05:45 +0000

    printmat padding

--- D2/custom.py
+++ D2/custom.py
@@ -1,9 +1,25 @@
-def printmat(mat):
+def printmat(mat, pad=" ", newline=True, min_len=1):
+    pad_num = min_len + 0
     if iter(mat):
-        for row in mat:
-            print(row)
-        
-        print("")
+        try:
+            iter(mat[0])
+        except TypeError:
+            for item in mat:
+                if len(str(item)) > pad_num:
+                    pad_num = len(str(item))
+
+            out_items = ""
+            for item in mat:
+                out_items += str(item)\
+                      + pad*(pad_num-len(str(item))) + ", "
+
+            print(f"[{out_items[:-2]}]")
+        else:
+            for item in mat:
+                printmat(item, pad=pad, newline=False, min_len=pad_num)
 
     else:
         print(mat)
+
+    if newline:
+        print()
